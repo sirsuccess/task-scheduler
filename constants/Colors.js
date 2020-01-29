3c7b1e32a9dbb970d2f631be9f774d6ebed908c4
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-29 14:17:47 +0100

    added some basic colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,28 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: "#ccc",
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  tabBar: "#fefefe",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeText: "#fff",
+  white: "#FFFFFF",
+  lightGray: "#00000017",
+  gray: "#646A65",
+  green1: "#75E977",
+  green2: "#70E56D",
+  green3: "#5EE555",
+  green4: "#2DE427",
+  green5: "#8DEF6F",
+  green: "#40C518",
+  lightGreen: "#A8F192",
+  deepGreen: "#063008",
+  lightDark: "#646A65",
+  dark50: "#707070",
+  lightBlue: "0097fe"
 };
